9bbac6847e0f47e85bb920d6fbcbcec87ca17230
cysof <cp4reallife@gmail.com>
2022-07-22 13:27:08 +0000

    secret key same dir

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = ''
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
